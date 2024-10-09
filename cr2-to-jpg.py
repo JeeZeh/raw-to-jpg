02be9155f079062614dfd2c4285173598bb8995a
Jesse Ashmore <jesse.ashmore97@gmail.com>
2024-10-09 23:43:46 +0100

    feat: upgrade to Python 3 and remove rawkit and numpy

--- cr2-to-jpg.py
+++ cr2-to-jpg.py
@@ -1,60 +1,77 @@
-#!/usr/bin/env python
-
-# import what we need
-import numpy
+from pathlib import Path
 import os
-import glob
-import time
+import tqdm
 import argparse
 from PIL import Image
-from rawkit import raw
 
 # params
-parser = argparse.ArgumentParser(description='Convert CR2 to JPG')
-parser.add_argument('-s', '--source', help='Source folder of CR2 files', required=True)
-parser.add_argument('-d', '--destination', help='Destination folder for converted JPG files', required=True)
+parser = argparse.ArgumentParser(description="Convert RAW to JPG")
+parser.add_argument(
+    "-s",
+    "--source",
+    help="Source folder of RAW files",
+    # required=True,
+    default="C:\\Users\\jesse\\OneDrive\\Pictures\\Camera\\",
+)
+parser.add_argument(
+    "-d",
+    "--destination",
+    help="Destination folder for converted JPG files",
+    # required=True,
+    default="C:\\Users\\jesse\\Desktop\\Camera\\",
+)
+parser.add_argument(
+    "-r",
+    "--raw_ext",
+    help="RAW format extension (e.g. CR2)",
+    # required=True,
+    default="CR2",
+)
+parser.add_argument(
+    "-t",
+    "--preserve-times",
+    help="Preserve the create and modified date for images.",
+    action="store_true",
+    default=False,
+)
+parser.add_argument(
+    "-e",
+    "--preserve-exif",
+    help="Preserve the EXIF information for images.",
+    action="store_true",
+    default=False,
+)
 args = parser.parse_args()
 
 # dirs and files
 raw_file_type = ".CR2"
-raw_dir = args.source + '/'
-converted_dir = args.destination + '/'
-raw_images = glob.glob(raw_dir + '*' + raw_file_type)
+raw_dir = Path(args.source)
+converted_dir = Path(args.destination)
+raw_images = raw_dir.glob(f"**/*.{args.raw_ext}")
 
-# converter function which iterates through list of files
-def convert_cr2_to_jpg(raw_images):
-    for raw_image in raw_images:
-        print "Converting the following raw image: " + raw_image + " to JPG"
 
-        # file vars
-        file_name = os.path.basename(raw_image)
-        file_without_ext = os.path.splitext(file_name)[0]
-        file_timestamp = os.path.getmtime(raw_image)
+def compress_raw(raw_file: Path, source_root: Path, destination_root: Path):
+    # file vars
+    file_timestamp = os.path.getctime(raw_file.resolve())
 
-        # parse CR2 image
-        raw_image_process = raw.Raw(raw_image)
-        buffered_image = numpy.array(raw_image_process.to_buffer())
+    # parse CR2 image
+    img = Image.open(raw_file.resolve())
+    dest = (destination_root / raw_file.relative_to(source_root)).with_suffix(".jpg")
+    dest.parent.mkdir(exist_ok=True, parents=True)
 
-        # check orientation due to PIL image stretch issue
-        if raw_image_process.metadata.orientation == 0:
-            jpg_image_height = raw_image_process.metadata.height
-            jpg_image_width = raw_image_process.metadata.width
-        else:
-            jpg_image_height = raw_image_process.metadata.width
-            jpg_image_width = raw_image_process.metadata.height
+    with dest.open(mode="w+") as out:
+        img.save(
+            out,
+            format="JPEG",
+            quality=95,
+            exif=img.getexif() if args.preserve_exif else b"",
+        )
 
-        # prep JPG details
-        jpg_image_location = converted_dir + file_without_ext + '.jpg'
-        jpg_image = Image.frombytes('RGB', (jpg_image_width, jpg_image_height), buffered_image)
-        jpg_image.save(jpg_image_location, format="jpeg")
+    if args.preserve_times:
+        os.utime(dest, (raw_file.lstat().st_ctime, raw_file.lstat().st_mtime))
 
-        # update JPG file timestamp to match CR2
-        os.utime(jpg_image_location, (file_timestamp,file_timestamp))
 
-        # close to prevent too many open files error
-        jpg_image.close()
-        raw_image_process.close()
-
-# call function
 if __name__ == "__main__":
-    convert_cr2_to_jpg(raw_images)
+    raws = [raw for raw in raw_images]
+    for raw in tqdm.tqdm(raws):
+        compress_raw(raw, source_root=raw_dir, destination_root=converted_dir)
